f9756aecfc32f3252e952064d6ee1c22b6f06c1a
candiedoperation <github-commits@atheesh.org>
2026-09-21 01:24:06 -0700

    Fixed Build Error w/ Docs

--- docs/components/search.tsx
+++ docs/components/search.tsx
@@ -11,11 +11,11 @@ import {
     type SharedProps,
 } from 'fumadocs-ui/components/dialog/search';
 import { useDocsSearch } from 'fumadocs-core/search/client';
-import { oramaStaticClient } from 'fumadocs-core/search/client/orama-static';
+import { staticClient } from 'fumadocs-core/search/client/orama-static';
 import { create } from '@orama/orama';
 import { useI18n } from 'fumadocs-ui/contexts/i18n';
 
-function initOrama() {
+function initOrama(): any {
     return create({
         schema: { _: 'string' },
         // https://docs.orama.com/docs/orama-js/supported-languages
@@ -26,7 +26,7 @@ function initOrama() {
 export default function DefaultSearchDialog(props: SharedProps) {
     const { locale } = useI18n(); // (optional) for i18n
     const { search, setSearch, query } = useDocsSearch({
-        client: oramaStaticClient({
+        client: staticClient({
             initOrama,
             locale,
         }),
